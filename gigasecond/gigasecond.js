08195bebbdb10d75cdb86530c65c141fc72469d9
denis2710 <denispereiraalves@gmail.com>
2019-05-23 12:44:56 -0300

    Adicionando GigaSecond

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -1,22 +1,9 @@
-class GigaSecond { 
-  gigasecond (date) {
-    let gs = new Date(null).setSeconds(Math.pow(10, 9))
-    console.log({gs})
-    // return date.setSeconds(date.getTime() + gs.getTime() )
- 
+'use strict'
+
+module.exports = { 
+  gigasecond: (birthday) => { 
+    const seconds = birthday.getSeconds() + 1000000000
+    birthday.setSeconds(seconds)
+    return birthday
   }
 }
-
-const gs = new GigaSecond()
-const dt = gs.gigasecond(new Date(Date.UTC(2015, 8, 14)));
-const expect = new Date(Date.UTC(2047, 4, 23, 1, 46, 40))
-
-
-
-console.log(dt)
-console.log(expect)
-
-
-
-
-
---
+++ two-fer/two-fer.js
@@ -0,0 +1,8 @@
+'use strict'
+
+module.exports = {
+  twoFer:  (name) => { 
+    name = name || 'you'
+    return `One for ${name}, one for me.`
+  }
+} 
